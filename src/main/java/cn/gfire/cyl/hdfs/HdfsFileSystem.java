5d7f881257e6f0e414d384d7b99e5d9634b752aa
iluvsnail <iluvsnail@gmail.com>
2015-11-13 15:45:22 +0800

    提交代码，部分有误

--- src/main/java/cn/gfire/cyl/hdfs/HdfsFileSystem.java
+++ src/main/java/cn/gfire/cyl/hdfs/HdfsFileSystem.java
@@ -1,15 +1,20 @@
 package cn.gfire.cyl.hdfs;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
 
 public class HdfsFileSystem {
 
-	public static void uploadFile(String src,String dst) throws IOException {
+	public static void uploadFile(String src, String dst) throws IOException {
 		Configuration conf = new Configuration();
 		FileSystem fs = FileSystem.get(conf);
 		Path srcPath = new Path(src); // 原路径
@@ -24,5 +29,32 @@ public class HdfsFileSystem {
 			System.out.println(file.getPath());
 		}
 		fs.close();
+	}
+
+	public static void downLoadFile(String src, String dst) throws IOException {
+		Configuration conf = new Configuration();
+		FileSystem fs = FileSystem.get(conf);
+		Path srcPath = new Path(src); // 原路径
+		InputStream in = null;
+		try {
+			in = fs.open(srcPath);
+			FileOutputStream fis=new FileOutputStream(dst);
+		//	fis.write();
+			fis.close();
+		} finally {
+			in.close();
+			fs.close();
+		}
+	}
+
+	public static void ls(String filePath) throws IOException {
+		Configuration conf = new Configuration();
+		FileSystem fs = FileSystem.get(conf);
+		Path path = new Path(filePath);
+		RemoteIterator<LocatedFileStatus> it = fs.listFiles(path, false);
+		if (it.hasNext()) {
+			System.out.println(it.next().getPath());
+		}
+
 	}
 }
--- src/test/java/cn/gfire/cyl/hdfs/HdfsFileSystemTest.java
+++ src/test/java/cn/gfire/cyl/hdfs/HdfsFileSystemTest.java
@@ -1,14 +1,29 @@
 package cn.gfire.cyl.hdfs;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.junit.Test;
+
+import junit.framework.Assert;
 
 public class HdfsFileSystemTest {
 
 	@Test
 	public void testUploadFile() throws IOException {
-		HdfsFileSystem.uploadFile("C:\\Users\\Acer\\Desktop\\chart.pdf", "hdfs://master:9000/chenyl/test/");
+		//HdfsFileSystem.uploadFile("C:\\Users\\Acer\\Desktop\\chart.pdf", "hdfs://master:9000/chenyl/test/");
 	}
 	
+	@Test
+	public void testDownloadFile() throws IOException {
+		HdfsFileSystem.downLoadFile("hdfs://master:9000/chenyl/test/chart.pdf", "C:\\");
+		File file = new File("C:\\chart.pdf");
+		Assert.assertTrue(file.exists());
+	}
+	
+	@Test
+	public void testLs() throws IOException{
+		String filePath = "hdfs://master:9000/chenyl/test/chart.pdf";
+		HdfsFileSystem.ls(filePath);
+	}
 }
